a5288d13eff1d43bae603f261a7a1e171bb120a6
Monica Nabais <monicamnabais@gmail.com>
2018-05-22 16:28:45 +0100

    rails task manager exercise

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,43 @@
 class TasksController < ApplicationController
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_params
+    @task = Task.find(params[:id])
+  end
 end
